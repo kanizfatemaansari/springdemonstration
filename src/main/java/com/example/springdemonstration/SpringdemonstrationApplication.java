be6afd02033de85e251d22ecb5e64dbd10ef336f
fatema <fatema@gmail.com>
2023-07-02 17:48:33 +0530

    this is committed web project

---
+++ src/main/java/com/example/springdemonstration/SpringdemonstrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemonstration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemonstrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemonstrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdemonstration/SpringdemonstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemonstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemonstrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
